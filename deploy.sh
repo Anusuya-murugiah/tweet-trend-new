7a30b6741403d03269d5c371c3852feb8280d969
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2025-04-05 21:52:03 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kubectl apply -f namesp.yaml
+kubectl apply -f ns.yaml
 kubectl apply -f secret.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f deploy.yaml
+kubectl apply -f svc.yaml
